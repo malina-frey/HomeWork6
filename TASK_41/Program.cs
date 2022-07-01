8f2e800a9d08c68be07fa22b28c7347a38bf630a
alina <nokian73.1991@mail.ru>
2022-07-01 17:11:42 +0300

    homework6

---
+++ TASK_41/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Сейчас будем вводить числа и считать сколько из них больше нуля! Сколько чисел будем вводить ? : ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ОК!Вводи числа!");
+
+int FillNumber(int [] array1)
+{
+    int Count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        array1[i] = Convert.ToInt32(Console.ReadLine());
+        if (array1[i] > 0)
+        {
+            Count++;
+        }
+    }
+    return Count;
+}
+
+void PrintArray(int [] array2)
+{
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write(array2[i]);
+        if (i != M-1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+int[] array = new int[M];
+int result = FillNumber(array);
+PrintArray(array);
+Console.Write(" -> " + result);
---
+++ TASK_43/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.WriteLine("Есть 2 прямые: y = k1 * x + b1   и   y = k2 * x + b2  ");
+Console.WriteLine("От вас требуется задать значения b1; k1 и b2; k2 , что-бы найти точку пересечения двух прямых");
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+void FindPoit()
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны  ");
+    }
+    else
+    {
+        double Xcord = ((b1 - b2) * -1) / (k1 - k2);
+        double Ycord = k1 * Xcord + b1;
+        Console.WriteLine("Координаты пересечения прямых: ");
+        Console.WriteLine("X = " + Xcord);
+        Console.WriteLine("Y = " + Ycord);
+    }
+}
+
+FindPoit();
